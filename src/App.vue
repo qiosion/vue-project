54e379dbe34dc22879af3252c634fbb9037f50bb
SUN <qiosion@gmail.com>
2023-08-24 14:47:20 +0900

    vuejs 프로젝트 생성

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ButtonCounter from './components/ButtonCounter.vue'
+</script>
+
+<template>
+  <h1>아래에 자식 컴포넌트가 있습니다</h1>
+  <ButtonCounter />
+
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ButtonCounter.vue
@@ -0,0 +1,12 @@
+<script setup>
+// 반응형 변수 ref
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+    <button @click="count++">당신은 {{ count }}번 클릭했습니다.</button>
+    <p>카운트 {{ count * 100 }}</p>
+
+</template>
