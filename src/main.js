60b620471926d706cdbe88e7f8cffee63f041da7
SUN <qiosion@gmail.com>
2023-08-31 11:40:27 +0900

    vuetify 세팅

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+import { createVuetify } from 'vuetify'
+
+const vuetify = createVuetify()
+
+createApp(App).use(vuetify).mount('#app')
