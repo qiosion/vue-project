8139e6d28ffd997834d66023477dc5bb42cf20e5
SUN <qiosion@gmail.com>
2023-09-07 12:20:44 +0900

    인스타그램 카드 예제

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,9 @@
 <script setup>
-  import ReactiveObj from './components/ReactiveObj.vue';
-  import RefObj from './components/RefObj.vue';
-  import ComputedProp from './components/ComputedProp.vue';
-  import CachedPropFunc from './components/CachedPropFunc.vue';
-  import WritableComputed from './components/WritableComputed.vue';
-  import VueList from './components/VueList.vue';
-  import ClassBind from './components/ClassBind.vue';
-  import ArrayClassBind from './components/ArrayClassBind.vue';
-  import MyComponent from './components/MyComponent.vue';
-  import MyMultiComponent from './components/MyMultiComponent.vue';
-  import StyleBind from './components/StyleBind.vue';
-  import TextFormBind from './components/TextFormBind.vue';
-  import CheckboxFormBind from './components/CheckboxFormBind.vue';
-  import RadioSelectFormBind from './components/RadioSelectFormBind.vue';
+  import Instagram from './components/Instagram.vue';
 </script>
 
 <template>
-  <!-- <v-btn icon="$vuetify"></v-btn> -->
-  <!-- <ReactiveObj/> -->
-  <!-- <RefObj/> -->
-  <!-- <ComputedProp/> -->
-  <!-- <CachedPropFunc/> -->
-  <!-- <WritableComputed/> -->
-  <!-- <VueList/> -->
-  <!-- <ClassBind/> -->
-  <!-- <ArrayClassBind/> -->
-  <!-- <MyComponent class="baz boo" />
-  <MyMultiComponent class="baz"/> -->
-  <!-- <StyleBind/> -->
-  <!-- <TextFormBind/> -->
-  <!-- <CheckboxFormBind/> -->
-  <RadioSelectFormBind/>
+  <Instagram/>
 </template>
 
 <style scoped>
