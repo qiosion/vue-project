d068cb1c6504015cefd2a5da3ba51e93a2d502a7
SUN <qiosion@gmail.com>
2023-09-07 10:51:52 +0900

    인라인스타일-객체로 바인딩

---
+++ src/components/StyleBind.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import { ref, reactive } from 'vue'
+
+    const activeColor = ref('red')
+    const fontSize = ref(30)
+
+    const styleObject = reactive({
+        color: 'red',
+        fontSize: '13px'
+    })
+
+    const toggle = () => {
+        fontSize.value++;
+        styleObject.fontSize = parseInt(styleObject.fontSize.trimEnd('px')) + 1 + 'px'
+    }
+</script>
+
+<template>
+    <div class="ma-2" :style="{ color: activeColor, fontSize: fontSize + 'px' }">바인딩 #1</div>
+    <div class="ma-2" :style="{ 'font-size': fontSize + 'px'}">바인딩 #2</div>
+    <div class="ma-2" :style="styleObject">바인딩 #3</div>
+    <v-btn @click="toggle">버튼</v-btn>
+</template>
+
