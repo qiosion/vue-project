99092c65f61477fc0f96c0f454d21b6b87a30cbe
SUN <qiosion@gmail.com>
2023-09-14 14:52:10 +0900

    컴포넌트 이벤트 청취하기

---
+++ src/components/BlogPageEmit.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import { ref } from 'vue';
+    import BlogPostEmit from './BlogPostEmit.vue';
+
+    const postFontSize = ref(1)
+    const posts = ref([
+        { id: 1, title: "Vue와 함께한 나의 여행" },
+        { id: 2, title: "Vue로 블로깅하기" },
+        { id: 3, title: "Vue가 재미없는 이유" }
+    ])
+</script>
+
+<template>
+    <div :style="{ fontSize: postFontSize + 'em'}">
+        <BlogPostEmit
+            v-for="post in posts"
+            :key="post.id"
+            :title="post.title"
+            @enlarge-text="postFontSize += 0.1"
+            @reduce-text="postFontSize -= 0.1"
+		/>
+    </div>
+</template>
+
+<style scoped>
+    .blog-post {
+        margin: 10px;
+        border: 1px solid;
+        padding: 5px;
+    }
+</style>
---
+++ src/components/BlogPostEmit.vue
@@ -0,0 +1,30 @@
+<script setup>
+    defineProps(['title'])
+    const emit = defineEmits(['enlarge-text', 'reduce-text'])
+
+    const enlarge = () => {
+        // $emit('enlarge-text')
+        emit('enlarge-text')
+    }
+
+    const reduce = () => {
+        emit('reduce-text')
+    }
+</script>
+
+<template>
+    <div class="blog-post">
+        <h4>{{ title }}</h4>
+        <v-btn @click="$emit('enlarge-text')">텍스트 확대</v-btn>
+        <!-- <v-btn @click="enlarge">텍스트 확대</v-btn> -->
+        <v-btn @click="$emit('reduce-text')">텍스트 축소</v-btn>
+    </div>
+</template>
+
+<style scoped>
+    .blog-post {
+        margin: 10px;
+        border: 1px solid;
+        padding: 5px;
+    }
+</style>
