4d08e56e6ef63fe78169f0a22fb46b9e0e49e9da
SUN <qiosion@gmail.com>
2023-09-14 14:51:20 +0900

    컴포넌트 슬롯

---
+++ src/components/ComponentSlot.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import AlertBox from './AlertBox.vue';
+</script>
+
+<template>
+    <AlertBox>
+        좋은 일이 일어났습니다.
+    </AlertBox>
+    <AlertBox>
+        <v-btn icon="mdi-magnify"></v-btn>
+    </AlertBox>
+</template>
