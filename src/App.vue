f8087e028f7793870c943aed8bbdd5c383e95d9c
SUN <qiosion@gmail.com>
2023-09-14 14:52:49 +0900

    컴포넌트

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
   import Instagram from './components/Instagram.vue';
+  import BlogPage from './components/BlogPage.vue';
+  import BlogPageVFor from './components/BlogPageVFor.vue';
+  import BlogPageEmit from './components/BlogPageEmit.vue';
+  import ComponentSlot from './components/ComponentSlot.vue';
 </script>
 
 <template>
-  <Instagram/>
+  <!-- <Instagram/> -->
+  <!-- <BlogPage/> -->
+  <!-- <BlogPageVFor/> -->
+  <!-- <BlogPageEmit/> -->
+  <ComponentSlot/>
 </template>
 
 <style scoped>
